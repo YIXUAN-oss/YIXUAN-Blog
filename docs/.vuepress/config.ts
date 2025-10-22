c5ba086b0e41f6871de45552adf05449707539dd
YIXUAN <byyi.xuan@outlook.com>
2025-10-22 20:29:37 +0800

    fix: remove algolia facetFilters

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -196,7 +196,7 @@ export default defineUserConfig({
                     }
                 }
             },
-            debug: false // 调试时可设置为 true
+            debug: true // 调试时可设置为 true
         },
 
         // 博客配置
